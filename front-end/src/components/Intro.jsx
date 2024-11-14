b9a34e61d743c3daa517ee14a271c6ca3b4894ac
Łukasz <98317104+Vneder@users.noreply.github.com>
2024-11-14 10:15:10 +0100

    Add files to components

---
+++ front-end/src/components/Intro.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+export function Intro() {
+  return (
+    <section className="intro">
+      <h2>Witaj w NOTEX</h2>
+      <p>Zorganizuj swoje notatki w jednym miejscu, bezpiecznie i wygodnie.</p>
+      <a href="register.html" className="cta-button">Rozpocznij teraz</a>
+    </section>
+  );
+};
+
