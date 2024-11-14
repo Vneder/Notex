5193424c921745fe3424897abe04869f300fb579
Łukasz <98317104+Vneder@users.noreply.github.com>
2024-11-14 09:47:57 +0100

    Update App.jsx
    Update app.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,59 @@
-import FoldersList from "./components/folders-list/FoldersList";
-import Layout from "./components/layout/Layout";
+// import Router from 'react';
+import { Header } from "./components/Header/Header";
+// import { Intro } from "./components/Intro/Intro";
+import { Features } from "./components/Features/Features";
+import { Footer } from "./components/Footer/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { Register } from "./components/Register/Register";
+import "./App.css";
 
-export function App() {
-    return (
-        <Layout>
-            <FoldersList />
-        </Layout>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <main>
+          <Routes>
+            {/* <Route path="/" element={<Intro />} /> */}
+            <Route path="/features" element={<Features />} />
+            {/* <Route path="/login" element={<Login />} /> */}
+            {/* <Route path="/register" element={<Register />} /> */}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
+
+// import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
+// import { Header } from "./components/Header/Header";
+// import { Intro } from "./components/Intro/Intro";
+// import { Features } from "./components/Features/Features";
+// import { Footer } from "./components/Footer/Footer";
+// import { Login } from "./components/Login/Login";
+// import { Register } from "./components/Register/Register"; 
+// import "./App.css";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <Header />
+//         <main>
+//           <Routes>
+//             <Route path="/" element={<Intro />} />
+//             <Route path="/features" element={<Features />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route path="/register" element={<Register />} />
+//           </Routes>
+//         </main>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
